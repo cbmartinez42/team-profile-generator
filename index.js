feb2115a63e79d80f9a8cb761d1881abf48f42a1
Chris Martinez <cbmartinez42@gmail.com>
2021-04-08 15:52:42 -0500

    updating with organized file/directory structure

--- index.js
+++ index.js
@@ -5,62 +5,8 @@ const jest = require('jest');
 
 const employees = [];
 
-function addManager() {
-    inquirer.prompt([
-        {
-            type: 'input',
-            message: "Enter Manager's name: ",
-            name: 'manager'
-        },
-        {
-            type: 'input',
-            message: 'Employee ID: ',
-            name: 'id'
-        },
-        {
-            type: 'input',
-            message: "Enter manager's email: ",
-            name: 'email'
-        },
-        {
-            type: 'input',
-            message: "Enter manager's office phone number: ",
-            name: 'phone'
-        }
-]) 
-} return manager;
 
-function addMember() {
-    inquirer.prompt([
-        {
-            type: 'input',
-            message: "Enter team member's name: ",
-            name: 'name'
-        },
-        {
-            type: 'list',
-            message: 'Select type of employee: ',
-            choices: ['Engineer', 'Intern'],
-            default: 0,
-            name: 'type'
-        },
-        {
-            type: 'input',
-            message: 'Employee ID: ',
-            name: 'id'
-        },
-        {
-            type: 'input',
-            message: "Enter member's email: ",
-            name: 'email'
-        },
-        {
-            type: 'input',
-            message: "Enter member's Github profile: ",
-            name: 'github'
-        }
-    ])
-};
+
 
 function createTeam() {
     addManager();
